5c4358012f0b1d96a695c32903dc735b72e14f6d
Thalia <thaliadepazr@gmail.com>
2022-01-19 15:14:59 -0500

    Se actualiza el test computer

--- cypress/integration/seccion_1/Hola_mundo.js
+++ cypress/integration/seccion_1/Hola_mundo.js
@@ -1,13 +1,6 @@
 describe("Bienvenido Modulo1", ()=>{
-    it('Primer Test -> Hola Mundo', () => {
+    it.only('Primer Test -> Hola Mundo', () => {
         cy.log("Holaaaaaa");
         cy.wait(1500);
     });
 })
-
-it('Segundo Test -> Campo name', () => {
-    cy.visit("https://demoqa.com/text-box");
-    cy.get("#userName").type("Sara");
-    cy.wait(4000);
-});
-
